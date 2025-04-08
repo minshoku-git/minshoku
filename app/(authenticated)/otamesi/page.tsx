5f2376cd46e2bcaf2db3d1cc62de88b5d771614c
sayuriabe101 <s.abe@refact.co.jp>
2025-04-08 13:02:52 +0900

    v0.0.0 鋭意作成中...

---
+++ app/(authenticated)/otamesi/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+import * as React from 'react';
+
+import { TestComponent } from './component';
+
+// ReservationManagement/予約管理画面
+export default function Page() {
+  return <TestComponent />;
+}
