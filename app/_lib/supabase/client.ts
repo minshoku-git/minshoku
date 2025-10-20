497a9b1398a2ae05dfafbe16d4ee31d183403b55
sayuriabe101 <s.abe@refact.co.jp>
2025-10-20 16:41:38 +0900

    v0.0.0 スキーマ切り替え処理をbrowserにも追加

--- app/_lib/supabase/client.ts
+++ app/_lib/supabase/client.ts
@@ -1,3 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr';
 
-export const createClient = () => createBrowserClient(process.env.SUPABASE_URL_DEV!, process.env.SUPABASE_ANON_DEV!);
+export const createClient = () =>
+  createBrowserClient(process.env.SUPABASE_URL_DEV!, process.env.SUPABASE_ANON_DEV!, {
+    db: { schema: process.env.SUPABASE_DB_SCHEMA },
+  });
