5c42dd769bf737c6bf4eabc7d668cd9ce40131cd
y_yoneyama <y.yoneyama@refact.co.jp>
2026-06-24 14:44:54 +0900

    y.y 店舗新規ロジック改修

--- app/(private)/shop-detail/[id]/_lib/types.ts
+++ app/(private)/shop-detail/[id]/_lib/types.ts
@@ -35,7 +35,7 @@ export type ShopDetailInitValues = z.infer<typeof ShopDetailInitSchema>;
 export const ShopDetailSchema = z
   .object({
     /** 店舗ID */
-    id: z.string().optional(),
+    id: z.string().optional().nullable(),
     /** 店舗名 */
     shop_name: z
       .string()
